eefb2d04818c8c4192f7f635b896bb2def578924
venkatangaluri4-byte <venkatangaluri4@gmail.com>
2026-04-17 00:30:49 -0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,7 @@
 const SUPABASE_URL = "https://kyyspgesdvpzajvoaoir.supabase.co";
 const SUPABASE_KEY = "sb_publishable_uxbSDRGHJS84hhc-kvOaQw_yINaoJHY";
 
-// Create client directly (no condition)
+// Directly create client
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-// Debug
 console.log("Supabase client created:", window.supabaseClient);
