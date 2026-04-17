7b42f0e2d5b7e5ec255bd5fb6d3105bd3aef03b5
venkatangaluri4-byte <venkatangaluri4@gmail.com>
2026-04-16 23:10:20 -0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://kyyspgesdvpzajvoaoir.supabase.co";
 
-const SUPABASE_KEY = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
+const SUPABASE_KEY = "sb_publishable_uxbSDRGHJS84hhc-kvOaQw_yINaoJHY";
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
